0e87ddaae58798fb0c98a32956ef479ce52e17dd
KimDaegyu <ddk3207@naver.com>
2025-12-15 10:57:44 +0900

    feat: Storybook 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { configDefaults } from 'vitest/config'
 import { VitePWA } from 'vite-plugin-pwa'
 import svgr from 'vite-plugin-svgr'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
+import { playwright } from '@vitest/browser-playwright'
+const dirname =
+    typeof __dirname !== 'undefined'
+        ? __dirname
+        : path.dirname(fileURLToPath(import.meta.url))
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
     plugins: [
         react(),
@@ -59,7 +69,12 @@ export default defineConfig({
         })
     ],
     resolve: {
-        alias: [{ find: '@', replacement: '/src' }]
+        alias: [
+            {
+                find: '@',
+                replacement: '/src'
+            }
+        ]
     },
     test: {
         environment: 'jsdom',
@@ -72,7 +87,33 @@ export default defineConfig({
         },
         browser: {
             enabled: true
-        }
+        },
+        projects: [
+            {
+                extends: true,
+                plugins: [
+                    // The plugin will run tests for the stories defined in your Storybook config
+                    // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+                    storybookTest({
+                        configDir: path.join(dirname, '.storybook')
+                    })
+                ],
+                test: {
+                    name: 'storybook',
+                    browser: {
+                        enabled: true,
+                        headless: true,
+                        provider: playwright({}),
+                        instances: [
+                            {
+                                browser: 'chromium'
+                            }
+                        ]
+                    },
+                    setupFiles: ['.storybook/vitest.setup.ts']
+                }
+            }
+        ]
     },
     server: {
         host: '0.0.0.0',
