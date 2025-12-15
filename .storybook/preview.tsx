b6834fc9120a85461396563d43054c6fd1c462be
KimDaegyu <ddk3207@naver.com>
2025-12-15 16:38:36 +0900

    chore: 스토리북 뷰포트 사이즈 조절 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../src/main.css";
-
+import { INITIAL_VIEWPORTS } from 'storybook/viewport';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -12,9 +12,35 @@ const queryClient = new QueryClient({
     },
   },
 });
-
+const customViewports = {
+  iphoneSE: {
+    name: 'iPhone SE (375x667)',
+    styles: {
+      width: '375px',
+      height: '667px',
+    },
+    type: 'mobile',
+  },
+  largeMobile: {
+    name: 'Large Mobile (414px)',
+    styles: {
+      width: '414px',
+      height: '896px',
+    },
+    type: 'mobile',
+  },
+}
 const preview: Preview = {
   parameters: {
+     viewport: {
+      viewports: {  
+        options: INITIAL_VIEWPORTS,
+        ...customViewports
+      }
+    },
+    initialGlobals:{
+      viewport: { value: 'mobile1', isRotated: false },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
