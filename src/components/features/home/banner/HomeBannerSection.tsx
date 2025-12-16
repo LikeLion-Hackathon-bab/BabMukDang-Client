d7591bc4f1e6f7ea8406cae8ff8c0da769937979
조아 <parkjoah@naver.com>
2025-12-17 00:15:49 +0900

    feat: 오늘의 공고 배너 슬라이드 구현 - useCarousel hook

--- src/components/features/home/banner/HomeBannerSection.tsx
+++ src/components/features/home/banner/HomeBannerSection.tsx
@@ -1,5 +1,11 @@
 import { MenuSuggestSection } from '@/components/features/home/banner/MenuSuggestSection'
+import { HomeMeetingBanner } from './HomeMeetingBanner'
 
 export const HomeBannerSection = () => {
-    return <MenuSuggestSection />
+    return (
+        <div className="-mx-[20px] mb-[30px] flex flex-col gap-[16px]">
+            <MenuSuggestSection />
+            <HomeMeetingBanner />
+        </div>
+    )
 }
--- src/components/features/home/banner/MenuSuggestSection.tsx
+++ src/components/features/home/banner/MenuSuggestSection.tsx
@@ -4,7 +4,7 @@ export const MenuSuggestSection = ({
     menu?: string
 }) => {
     return (
-        <h1 className="text-body1-semibold -mx-[20px] flex h-[61px] items-center justify-center gap-[20px] bg-[linear-gradient(90deg,#FFB399_7.03%,#FF8359_30.09%,#FF7343_49.96%,#FF7B4E_71.58%,#FFA485_91.21%)] text-white">
+        <h1 className="text-body1-semibold flex h-[61px] items-center justify-center gap-[20px] bg-[linear-gradient(90deg,#FFB399_7.03%,#FF8359_30.09%,#FF7343_49.96%,#FF7B4E_71.58%,#FFA485_91.21%)] text-white">
             <span>오늘은</span>
             <span className="text-body1-medium">[</span>
             <span className="text-title1-bold">{menu}</span>
