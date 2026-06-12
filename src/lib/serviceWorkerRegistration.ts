aac5bc8047efa93612f6e9507df416ebe62e6352
KimDaegyu <ddk3207@naver.com>
2026-06-13 01:20:17 +0900

    chore(client): shared 패키지와 서비스워커 설정 정리

--- src/lib/serviceWorkerRegistration.ts
+++ src/lib/serviceWorkerRegistration.ts
@@ -1,30 +1,21 @@
 // import { registerSW } from 'virtual:pwa-register'
 
-// const updateSW = registerSW({
-//     onNeedRefresh() {
-//         console.log('Need refresh')
-//     },
-//     onOfflineReady() {
-//         console.log('Offline ready')
-//     }
-// })
-
 export const register = () => {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('/service-worker.js', {
-                scope: '/'
-            })
-            .then(registration => {
-                console.log(
-                    'Service worker registration succeeded',
-                    registration
-                )
-            })
-            .catch(error => {
-                console.error('Service worker registration failed: ', error)
-            })
-    } else {
-        console.log('Service workers are not supported.')
-    }
+    // if ('serviceWorker' in navigator) {
+    //     navigator.serviceWorker
+    //         .register('/service-worker.js', {
+    //             scope: '/'
+    //         })
+    //         .then(registration => {
+    //             console.log(
+    //                 'Service worker registration succeeded',
+    //                 registration
+    //             )
+    //         })
+    //         .catch(error => {
+    //             console.error('Service worker registration failed: ', error)
+    //         })
+    // } else {
+    //     console.log('Service workers are not supported.')
+    // }
 }
